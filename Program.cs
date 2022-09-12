becb9ee45cb6d603194b35b360f837f12aaaef9f
MatejPemper <90246393+MatejPemper@users.noreply.github.com>
2022-09-12 12:18:44 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicijalnoPonavljanje1._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 100;
+            int b = 3;
+
+            Console.WriteLine( (a / b) + " \nostatak je " + (a % b));
+
+            Console.ReadKey();
+
+        }
+    }
+}
